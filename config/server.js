df5a742a12dfd9852d314043f92abfffaaa9b42b
Henrique Suel <hsuel@TB000375.localdomain>
2021-02-13 15:50:30 -0300

    strapi finalizado

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '49563bbfde653865ef52ee30b38792fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2375992-626b-4c1d-b7d1-3e41da136dcd'
+};
